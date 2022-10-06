f305888ba00a1639bd50e381dec00636239b46b8
Mohammad Amin Seifinia <thewitcher745@gmail.com>
2022-10-06 23:43:28 +0330

    Added better error handling to postController

--- node-api/controllers/postController.js
+++ node-api/controllers/postController.js
@@ -2,33 +2,36 @@ const PostModel = require("../models/postModel");
 
 const getUserPosts = (req, res) => {
   PostModel.find({ postedBy: req.params.id })
+    .sort("createdAt")
     .then((posts) => {
       res.json({ posts });
     })
-    .catch((err) => console.log(err));
+    .catch((err) => res.status(400).json({ error: error }));
 };
 
 const getAllPosts = (req, res) => {
   PostModel.find()
+    .sort("createdAt")
     .then((posts) => {
       res.json({ posts });
     })
-    .catch((err) => console.log(err));
+    .catch((err) => res.status(400).json({ error: error }));
 };
 
 const createPost = (req, res, next) => {
   const post = new PostModel(req.body);
-  post.save().then((post) => console.log(post));
-
-  res.json(req.body);
+  post
+    .save()
+    .then((post) => res.json(post))
+    .catch((err) => res.status(400).json({ error: error }));
 };
 
 const removePost = (req, res) => {
   PostModel.deleteOne({ _id: req.body._id })
-    .then((posts) => {
-      res.status(200).json({ posts });
+    .then((post) => {
+      res.status(200).json({ post });
     })
-    .catch((err) => console.log(err));
+    .catch((err) => res.status(400).json({ error: error }));
 };
 
 module.exports = { getUserPosts, getAllPosts, createPost, removePost };
